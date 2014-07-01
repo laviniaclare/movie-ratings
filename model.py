2e112e86011867b1fafd3fcd6de76dee75c80de8
Hackbright Student <info@hackbrightacademy.com>
2014-06-30 18:09:00 -0700

    Finished part one.  Databases populated.  Woot

--- model.py
+++ model.py
@@ -2,12 +2,57 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import DateTime 
+
+
+ENGINE = None
+Session = None
+
 Base = declarative_base()
 
 ### Class declarations go here
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True)
+    gender = Column(String, nullable = True)
+    occupation = Column(String, nullable = True)
+    email = Column(String(64), nullable = True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable = True)
+    zipcode = Column(String(15), nullable=True)
+
+class Movie(Base):
+    __tablename__="movies"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String, nullable = True)
+    released_at= Column(DateTime, nullable = True)
+    imdb_url = Column(String, nullable = True)
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key = True)
+    movie_id = Column(Integer, nullable = True)
+    user_id = Column(Integer, nullable = True)
+    rating = Column(Integer, nullable = True)
+
+
+
+
 
 ### End class declarations
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo = True)
+    Session = sessionmaker(bind = ENGINE)
+
+    return Session()
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -1,21 +1,72 @@
 import model
 import csv
+import datetime
+
 
 def load_users(session):
-    # use u.user
-    pass
+    users_data=open('seed_data/u.user')
+    users_data = users_data.readlines()
+
+    for line in users_data:
+        row=line.split('|')
+        i=row[0].decode("latin-1")
+        a=row[1].decode('latin-1')
+        g=row[2].decode('latin-1')
+        o=row[3].decode('latin-1')
+        z=row[4].decode('latin-1')
+        user=model.User(id=i, age=a, gender=g, 
+                  occupation=o, zipcode=z)
+
+        session.add(user)
+    session.commit()
+
 
 def load_movies(session):
-    # use u.item
-    pass
+
+    movies_data=open('seed_data/u.item')
+    movies_data = movies_data.readlines()
+    
+    for line in movies_data:
+        row=line.split('|')
+        if row[1] != "unknown":
+            i=row[0].decode("latin-1")
+            #the line below removes the dates from the movie titles 
+            #and assigns the title to the variable 'n', in case you were curious
+            n=row[1].decode('latin-1')[:-6]
+            ra=row[2].decode('latin-1')
+            release_date=datetime.datetime.strptime(ra,"%d-%b-%Y")
+            imdb=row[3].decode('latin-1')
+
+            movie=model.Movie(id=i, name=n, released_at=release_date, 
+                  imdb_url=imdb)
+
+            session.add(movie)
+
+    session.commit() 
+
+
 
 def load_ratings(session):
-    # use u.data
-    pass
+
+    ratings_data=open('seed_data/u.data')
+    ratings_data = ratings_data.readlines()
+
+    for line in ratings_data:
+        row=line.split()
+        ui=row[0].decode("latin-1")
+        mi=row[1].decode('latin-1')
+        r=row[2].decode('latin-1')
+
+        rating=model.Rating(user_id=ui, movie_id=mi, rating=r)
+
+        session.add(rating)
+    session.commit()
 
 def main(session):
-    # You'll call each of the load_* functions with the session as an argument
-    pass
+   # load_users(session)
+   # load_movies(session)
+   # load_ratings(session)
+   pass
 
 if __name__ == "__main__":
     s= model.connect()
